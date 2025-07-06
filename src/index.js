a1b3a45c3fdcff19b33c8ae0fd6825d3215cb88e
Jane Vasylieva <janekanso86@gmail.com>
2025-07-06 16:59:32 +0300

    add

--- src/index.js
+++ src/index.js
@@ -2,9 +2,35 @@ import 'dotenv/config';
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
-const bootstrap = async () => {
-  await initMongoConnection();
-  setupServer();
+import Contact from './models/contact.js';
+
+const createContact = async () => {
+  const exists = await Contact.findOne({ phoneNumber: '+380501234567' });
+  if (exists) {
+    console.log('Контакт вже існує');
+    return;
+  }
+
+  const newContact = new Contact({
+    name: 'Іван Іваненко',
+    phoneNumber: '+380501234567',
+    email: 'ivan@example.com',
+    isFavourite: false,
+    contactType: 'personal',
+  });
+
+  await newContact.save();
+  console.log('✅ Контакт додано');
 };
 
+const bootstrap = async () => {
+  try {
+    await initMongoConnection();
+    await createContact();
+    setupServer();
+  } catch (error) {
+    console.error('❌ Помилка під час запуску:', error);
+    process.exit(1);
+  }
+};
 bootstrap();
