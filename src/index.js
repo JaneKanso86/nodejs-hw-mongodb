8c4149e87b407eb635d2312e660b92f2f03873e7
Jane Vasylieva <janekanso86@gmail.com>
2025-07-08 16:52:44 +0300

    add chng

--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ import 'dotenv/config';
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
-import Contact from './models/contact.js';
-
-const createContact = async () => {
-  const exists = await Contact.findOne({ phoneNumber: '+380501234567' });
-  if (exists) {
-    console.log('Контакт вже існує');
-    return;
-  }
-
-  const newContact = new Contact({
-    name: 'Іван Іваненко',
-    phoneNumber: '+380501234567',
-    email: 'ivan@example.com',
-    isFavourite: false,
-    contactType: 'personal',
-  });
-
-  await newContact.save();
-  console.log('✅ Контакт додано');
-};
-
 const bootstrap = async () => {
   try {
     await initMongoConnection();
--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ export const setupServer = () => {
   });
 
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, '0.0.0.0', () => {
+  app.listen(PORT, () => {
     console.log(`✅ Server is running on port ${PORT}`);
   });
 };
