7f5551a59af34581a0bea8a8b8b040b608c27bca
Jane Vasylieva <janekanso86@gmail.com>
2025-07-09 17:50:29 +0300

    add

--- src/models/contact.js
+++ src/models/contact.js
@@ -4,9 +4,7 @@ const contactSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     phoneNumber: { type: String, required: true },
-    email: {
-      type: String,
-    },
+    email: { type: String },
     isFavourite: { type: Boolean, default: false },
     contactType: {
       type: String,
