8620d4d5c09a899eb290fd162b8a4c4e0deef160
Jane Vasylieva <janekanso86@gmail.com>
2025-07-13 23:41:58 +0300

    add

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,35 +2,27 @@ import * as contactsService from '../services/contacts.js';
 import createError from 'http-errors';
 
 export const getContacts = async (req, res) => {
-  try {
-    const contacts = await contactsService.getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  const contacts = await contactsService.getAllContacts();
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
 };
 
 export const getContact = async (req, res) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await contactsService.getContactById(contactId);
+  const { contactId } = req.params;
+  const contact = await contactsService.getContactById(contactId);
 
-    if (!contact) {
-      return res.status(404).json({ message: 'Contact not found' });
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  if (!contact) {
+    throw createError(404, 'Contact not found');
   }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
 };
 
 export const createContact = async (req, res) => {
