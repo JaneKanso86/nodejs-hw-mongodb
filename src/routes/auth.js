d42e4672d8774217ab7287c50653bd8ebc8382ec
Jane Vasylieva <janekanso86@gmail.com>
2025-07-26 17:40:33 +0300

    add chn

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { loginUser } from '../controllers/auth.js';
+import { loginUser, logout, refreshSession } from '../controllers/auth.js';
+
 import { validateBody } from '../middlewares/validateBody.js';
 import { loginSchema } from '../validation/authSchemas.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { logout } from '../controllers/auth.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
 router.post('/login', validateBody(loginSchema), ctrlWrapper(loginUser));
 router.post('/refresh', ctrlWrapper(refreshSession));
-router.post('/logout', authenticate, logout);
+router.post('/logout', authenticate, ctrlWrapper(logout));
+
 export default router;
