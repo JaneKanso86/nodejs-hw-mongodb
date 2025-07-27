9558bdc2516c43d40d61eba7211d42d50e015b40
Jane Vasylieva <janekanso86@gmail.com>
2025-07-27 20:58:53 +0300

    add7

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -13,7 +13,7 @@ import { authenticate } from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post('/register', ctrlWrapper(registerUser));
 router.post('/login', validateBody(loginSchema), ctrlWrapper(loginUser));
 router.post('/refresh', ctrlWrapper(refreshSession));
 router.post('/logout', authenticate, ctrlWrapper(logout));
