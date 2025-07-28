ef2dafcef3dbae7c4121d96346736fb865b0b171
Jane Vasylieva <janekanso86@gmail.com>
2025-07-28 11:05:14 +0300

    add8

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
 import authRouter from './routes/auth.js';
 import cookieParser from 'cookie-parser';
 
 export function setupServer() {
   const app = express();
 
+  app.use(cors());
+  app.use(pino());
   app.use(express.json());
   app.use(cookieParser());
   app.use('/auth', authRouter);
